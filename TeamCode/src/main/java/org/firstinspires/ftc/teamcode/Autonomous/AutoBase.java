942af09f4f5c78d657f289ead763faa1007d337c
Stephen O <stephenconochie@gmail.com>
2024-09-21 10:36:00 -0400

    Added Autobase Program
    This program is used by all auto programs to access subsystem controls.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoBase.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+//import needed libraries
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.Subsystems.*;
+
+//this program is used by all auto programs to access subsystem controls
+public class AutoBase extends LinearOpMode {
+    //subsystem objects - intake, lift, slides, vision, etc
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+
+    //used by auto programs to init subsystems
+    public void init_classes(){
+
+    }
+}
