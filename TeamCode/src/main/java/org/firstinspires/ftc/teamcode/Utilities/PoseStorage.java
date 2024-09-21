38135fc7748d78f0677c9579dccb0e2c648c8766
Stephen O <stephenconochie@gmail.com>
2024-09-21 00:48:38 -0400

    Installed RoadRunner + Created PoseStorage Class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/PoseStorage.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.Utilities;
+
+import com.acmerobotics.roadrunner.Pose2d;
+
+/*
+ * a simple program to hold widely used poses on the field
+ * currentPose is a dynamic Pose2d variable for the robot's pose - often used to hand off robot's pose between opModes
+ * the other poses are final and are direct points on the field
+ */
+
+public class PoseStorage {
+
+    public static Pose2d currentPose = null;
+
+
+}
