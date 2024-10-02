c0596f4b57e4d0dac4e6c0e28f61184cef70997c
Stephen O <stephenconochie@gmail.com>
2024-10-02 15:01:03 -0400

    Initial Work towards Arm Class
    - Added default variables and method we'll use
    
    - PID variables MUST be constant
    
    - will create a tuning program for our arm later on in the season (will refer to last year 21)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Arm.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+//import needed libraries
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.arcrobotics.ftclib.controller.PIDFController;
+
+//robot subsystem for extendable arm
+public class Arm {
+    //motor & servo objects
+
+    //absolute positions ("final" means they can't change later in code)
+    private final double groundPose = 0; //TODO
+    private final double basket1Pose = 0; //TODO
+    private final double basket2Pose = 0; //TODO
+    private final double maxPose = 0; //TODO
+    private double currentPose = 0;
+    private Arm_Poses armState = null;
+
+    //TODO - PID variables
+    double target = 0;
+    private final double p = 0;
+    private final double i = 0;
+    private final double d = 0;
+    private final double f = 0;
+
+    //useful variables
+
+    //enum commands for arm positions
+    public enum Arm_Poses {
+        GROUND,
+        BASKET1,
+        BASKET2,
+        DRIVER_CONTROL,
+        MAX
+    }
+
+    //--------TELEOP COMMANDS---------
+    public void init(HardwareMap hardwareMap){
+
+    }
+
+    public void run_teleOp(GamepadEx driverOp){
+
+    }
+
+    //--------AUTO COMMANDS------------
+
+    //quick command to control arm
+    public void set_arm(Arm_Poses pose){
+
+    }
+
+    //precise command to control arm
+    public void set_arm(double pose){
+
+    }
+
+
+    public void getTelemetryBRIEF(Telemetry telemetry){
+        telemetry.addData("Arm Pose:", currentPose);
+    }
+
+    public void getTelemetryFULL(Telemetry telemetry){
+        telemetry.addData("Arm Pose:", currentPose);
+        telemetry.addData("Arm STATE:", armState);
+
+
+    }
+}
