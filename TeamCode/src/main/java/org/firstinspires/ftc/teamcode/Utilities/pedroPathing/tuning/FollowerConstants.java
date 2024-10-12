13e3d3b63ba4f648b54cc82c59ff2203d34e50af
Stephen O <stephenconochie@gmail.com>
2024-10-12 19:42:08 -0400

    Drive PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/pedroPathing/tuning/FollowerConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/pedroPathing/tuning/FollowerConstants.java
@@ -67,10 +67,10 @@ public class FollowerConstants {
 
     // Drive PIDF coefficients
     public static CustomFilteredPIDFCoefficients drivePIDFCoefficients = new CustomFilteredPIDFCoefficients(
-            0.025,
+            0.0045,
             0,
-            0.00001,
-            0.6,
+            0.0001,
+            0.65,
             0);
 
     // Feed forward constant added on to the drive PIDF
