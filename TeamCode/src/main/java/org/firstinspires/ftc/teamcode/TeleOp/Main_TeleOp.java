379963b442515dd26f11f44d9ebd586be55a7774
Stephen O <stephenconochie@gmail.com>
2024-10-12 10:51:56 -0400

    Cleared up Driver_Feedback + Added Blank Subsystems to Main TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Main_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Main_TeleOp.java
@@ -12,6 +12,11 @@ public class Main_TeleOp extends LinearOpMode {
 
     //create subsystem objects
     private final org.firstinspires.ftc.teamcode.Subsystems.Drivetrain drivetrain = new Drivetrain();
+//    private final org.firstinspires.ftc.teamcode.Subsystems.Arm arm = new Arm();
+//    private final org.firstinspires.ftc.teamcode.Subsystems.Intake intake = new Intake();
+//    private final org.firstinspires.ftc.teamcode.Subsystems.Lift lift = new Lift();
+//    private final org.firstinspires.ftc.teamcode.Subsystems.Computer_Vision vision = new Computer_Vision();
+//    private final org.firstinspires.ftc.teamcode.Subsystems.LED lighting = new LED();
     private final org.firstinspires.ftc.teamcode.Subsystems.Driver_Feedback feedback = new Driver_Feedback();
 
     //Driver gamepad objects - set to static so subsystems can access controls
@@ -34,6 +39,9 @@ public class Main_TeleOp extends LinearOpMode {
         while(opModeIsActive()){
             drivetrain.run_fieldCentric(Driver1Op, feedback);
             drivetrain.getTelemetryFULL(telemetry);
+
+            telemetry.addLine("---Endgame Timer---");
+            telemetry.addData("Time Left: ", feedback.getTimer());
             telemetry.update();
         }
     }
