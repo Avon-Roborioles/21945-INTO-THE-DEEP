624f87a2a24cd91243e141a8092943c0b04b0846
Stephen Onochie <stephenconochie@gmail.com>
2025-02-03 17:36:39 -0500

    Added Lift TeleOp Test
    - quick way to see if liftMotor needs to be in reverse

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/Tests/LiftTeleOpTest.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.Utilities.Tests;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class LiftTeleOpTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        GamepadEx driverOp = new GamepadEx(gamepad2);
+        MotorEx liftMotor = new MotorEx(hardwareMap,"liftMotor");
+        double rightY = driverOp.getRightY();
+
+        MultipleTelemetry mainTelemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+
+        liftMotor.setRunMode(Motor.RunMode.RawPower);
+
+        waitForStart();
+
+        while(opModeIsActive()){
+            if(rightY > 0){
+                liftMotor.set(0.8 * rightY);
+            } else if(rightY < 0){
+                liftMotor.set(-0.5 * Math.abs(rightY));
+            } else {
+                liftMotor.set(0);
+            }
+
+            mainTelemetry.addData("Lift Pose: ", liftMotor.getCurrentPosition());
+            mainTelemetry.addData("Lift Velocity: ", liftMotor.getVelocity());
+            mainTelemetry.update();
+        }
+    }
+}
